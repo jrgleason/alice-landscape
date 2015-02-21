e1747bcd9a305032a2c8ebee413903b57de332f3
Your Name <jackiegleason@gmail.com>
2015-02-21 16:22:15 -0500

    Some more stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,40 @@
-module.exports = function (grunt) {
-  grunt.initConfig({
-    compress: {
-      main: {
-        options: {
-          archive: 'alice-landscape.zip'
-        },
-        files: [
-          {src: ['./theme/**']}
-        ] 
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-compress');
+module.exports = function(grunt) {
+	grunt.initConfig({
+		compress : {
+			main : {
+				options : {
+					archive : 'alice-landscape.zip'
+				},
+				files : [ {
+					src : [ './theme/**' ]
+				} ]
+			}
+		},
+		html2js : {
+			build : {
+				src : [ 'src/main/jade/**/*.jade', 'src/main/html/**/*.html' ],
+				dest : 'build/alice-landscape-templates.js'
+			}
+		},
+		concat : {
+			core : {
+				src : [ 'src/main/angular/**/app.js',
+						'src/main/angular/**/*.js' ],
+				dest : 'build/alice-angular-core.js',
+			}
+		},
+		less : {
+			default:{
+			    files : {
+				    "theme/css/theme.css" : "src/main/less/main.less"
+			    }
+			}
+			
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-html2js');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-less');
 };
--- deploy-alice.sh
+++ deploy-alice.sh
@@ -1,2 +1,3 @@
 rm -rf /Library/WebServer/Documents/wordpress/wp-content/themes/alice-landscape
-cp -rf ./theme /Library/WebServer/Documents/wordpress/wp-content/themes/alice-landscape
+cp -rf ./theme /Library/WebServer/Documents/wordpress/wp-content/themes
+mv /Library/WebServer/Documents/wordpress/wp-content/themes/theme /Library/WebServer/Documents/wordpress/wp-content/themes/alice-landscape
