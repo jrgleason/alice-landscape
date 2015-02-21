c100ca9d9545ec66b2d094c5b5b78ac2b6758ff7
Your Name <jackiegleason@gmail.com>
2015-02-21 16:26:04 -0500

    Guess people don't really need this either

--- deploy-alice.sh
+++
@@ -1,3 +0,0 @@
-rm -rf /Library/WebServer/Documents/wordpress/wp-content/themes/alice-landscape
-cp -rf ./theme /Library/WebServer/Documents/wordpress/wp-content/themes
-mv /Library/WebServer/Documents/wordpress/wp-content/themes/theme /Library/WebServer/Documents/wordpress/wp-content/themes/alice-landscape
