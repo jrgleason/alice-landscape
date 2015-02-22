64288d4202b5e3ac692aaa39b8bfdccbb01cc004
Your Name <jackiegleason@gmail.com>
2015-02-22 15:39:40 -0500

    Some more styling stuff getting checked in

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -7,13 +7,13 @@
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php do_action( 'before_sidebar' ); ?>
-			<div id="left-sidebar">
+			<div id="left-sidebar" class="sidebar">
 			  <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 				<?php get_search_form(); ?>
 			  <?php endif; // end sidebar widget area ?>
 			</div><!-- #left-sidebar -->
 			
-			<div id="middle-sidebar">
+			<div id="middle-sidebar" class="sidebar">
 			<?php if ( ! dynamic_sidebar( 'sidebar-2' ) ) : ?>
 					<h1 class="widget-title"><?php _e( 'Archives', 'landscape' ); ?></h1>
 					<ul>
@@ -22,7 +22,7 @@
 			<?php endif; // end sidebar widget area ?>
 			</div><!-- #middle-sidebar -->
 			
-			<div id="right-sidebar">
+			<div id="right-sidebar" class="sidebar">
 			<?php if ( ! dynamic_sidebar( 'sidebar-3' ) ) : ?>
 					<h1 class="widget-title"><?php _e( 'Meta', 'landscape' ); ?></h1>
 					<ul>
